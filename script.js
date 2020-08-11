fd327b0248626d79195660203b7f3e126fb64b24
Jack Sorrels <sorrelsjack@gmail.com>
2020-08-11 17:42:28 -0400

    Can spin now

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let degrees = 360;
 let slices = ['PDQ', 'Chick-Fil-A', 'Smashburger', 'Taco Bell', 'KFC', 'McDonalds'];
-let colors = ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51']; // https://coolors.co/264653-2a9d8f-e9c46a-f4a261-e76f51
+//let colors = ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51']; // https://coolors.co/264653-2a9d8f-e9c46a-f4a261-e76f51
+let colors = ['#264653', '#e9c46a'];
 
 // Credit: http://jsbin.com/qefada/11/edit?html,js,output
 var sliceDeg = 360 / slices.length;
@@ -9,11 +10,15 @@ var ctx = null;
 var width = 0;
 var center = 0;
 
-function getRandomInt(min, max) {
+let rotateDegree = 0;
+
+const getRandomInt = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+const calculateSliceDeg = () => sliceDeg = 360 / slices.length;
 
 const degToRad = (deg) => deg * Math.PI / 180;
 
@@ -37,6 +42,11 @@ const drawText = (deg, text) => {
     ctx.restore();
 }
 
+const rotateChart = () => {
+    const c = document.getElementById('circle-canvas');
+    c.style.transform = `rotate(${rotateDegree += 2}deg)`;
+}
+
 const drawChart = () => {
     canvas = document.getElementById('circle-canvas');
     ctx = canvas.getContext('2d');
@@ -49,6 +59,8 @@ const drawChart = () => {
         drawText(degrees + sliceDeg / 2, slices[i]);
         degrees += sliceDeg;
     }
+
+    //window.setInterval(rotateChart, 100)
 }
 
 const initialize = () => {
@@ -56,7 +68,7 @@ const initialize = () => {
 }
 
 const getColors = () => {
-    // TODO: Avoid the same colors touching
+    // TODO: Stop colors from touching
     const numberOfColorSetsNeeded = Math.ceil(slices.length / colors.length);
     let sets = [];
 
@@ -71,8 +83,17 @@ const measureClickVelocity = () => {
     // TODO: In the background, go through the indexes at a comparable speed to the pie slices
 }
 
+const resetChart = () => {
+    calculateSliceDeg();
+    clearInterval();
+    drawChart();
+    rotateDegree = 0;
+}
+
 // TODO: Save places to local storage and load them when the page comes up
+// TODO: Add place list and the ability to delete places
 const handleAddClicked = () => {
     const place = document.getElementById('place-input').value;
     slices.push(place);
+    resetChart();
 }
