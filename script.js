4e4a6eef51b8cf4fdf657ff01bcbeb5a1643dc3b
Jack Sorrels <sorrelsjack@gmail.com>
2020-08-11 15:04:50 -0400

    Drawing the circle instead of using a package

--- script.js
+++ script.js
@@ -2,8 +2,59 @@ const degrees = 360;
 let slices = ['PDQ', 'Chick-Fil-A', 'Smashburger', 'Taco Bell', 'KFC', 'McDonalds'];
 let colors = ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51']; // https://coolors.co/264653-2a9d8f-e9c46a-f4a261-e76f51
 
+// Credit: http://jsbin.com/qefada/11/edit?html,js,output
+var sliceDeg = 360 / slices.length;
+var deg = getRandomInt(0, 360);
+var canvas = null;
+var ctx = null;
+var width = 0;
+var center = 0;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const degToRad = (deg) => deg * Math.PI / 180;
+
+const drawSlice = (deg, color) => {
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.moveTo(center, center);
+    ctx.arc(center, center, width / 2, degToRad(deg), degToRad(deg + sliceDeg));
+    ctx.lineTo(center, center);
+    ctx.fill();
+}
+
+const drawText = (deg, text) => {
+    ctx.save();
+    ctx.translate(center, center);
+    ctx.rotate(degToRad(deg));
+    ctx.textAlign = "right";
+    ctx.fillStyle = "#fff";
+    ctx.font = 'bold 20px sans-serif';
+    ctx.fillText(text, 130, 10);
+    ctx.restore();
+}
+
+const drawImg = () => {
+    canvas = document.getElementById('circle-canvas');
+    ctx = canvas.getContext('2d');
+    width = canvas.width;
+    center = width / 2;
+
+    ctx.clearRect(0, 0, width, width);
+    for (var i = 0; i < slices.length; i++) {
+        drawSlice(deg, getColors()[i]);
+        drawText(deg + sliceDeg / 2, slices[i]);
+        deg += sliceDeg;
+    }
+}
+
 const initialize = () => {
-    drawPieChart();
+    drawImg();
+    //drawPieChart();
 }
 
 const getChartValues = () => {
@@ -19,7 +70,7 @@ const getChartValues = () => {
 
 const getColors = () => {
     // TODO: Avoid the same colors touching
-    const numberOfColorSetsNeeded = Math.ceil(slices.length / colors.length) || 6;
+    const numberOfColorSetsNeeded = Math.ceil(slices.length / colors.length);
     let sets = [];
 
     for (let i = 0; i < numberOfColorSetsNeeded; i++) {
@@ -58,7 +109,7 @@ const drawPieChart = () => {
 }
 
 const measureClickVelocity = () => {
-
+    // TODO: In the background, go through the indexes at a comparable speed to the pie slices
 }
 
 // TODO: Save places to local storage and load them when the page comes up
