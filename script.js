e8478e524483db4a95c79342df4cfda17c2c0f42
Jack Sorrels <sorrelsjack@gmail.com>
2020-08-11 13:46:36 -0400

    Using chartjs instead

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
 const degrees = 360;
-let slices = [];
-let colors = [];
+let slices = ['a', 'a', 'a', 'a', 'a', 'a'];
+let colors = ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51']; // https://coolors.co/264653-2a9d8f-e9c46a-f4a261-e76f51
 
 const initialize = () => {
-    
+    drawPieChart();
+}
+
+const getChartValues = () => {
+    let values = [];
+    const percent = 1 / slices.length;
+
+    for (let i = 0; i < slices.length; i++) {
+        values.push(percent);
+    }
+
+    return values;
+}
+
+const getColors = () => {
+    // TODO: Avoid the same colors touching
+    const numberOfColorSetsNeeded = Math.ceil(slices.length / colors.length) || 6;
+    let sets = [];
+
+    for (let i = 0; i < numberOfColorSetsNeeded; i++) {
+        sets = sets.concat(colors);
+    }
+
+    return sets;
+}
+
+const drawPieChart = () => {
+    const context = document.getElementById("circle-canvas").getContext("2d");
+    const chart = new Chart(context, {
+        type: 'pie',
+        data: {
+            datasets: [{
+                data: getChartValues(),
+                backgroundColor: getColors()
+            }]
+        },
+        options: {
+            tooltips: {
+                enabled: false
+            }
+        }
+    })
+}
+
+const measureClickVelocity = () => {
+
+}
+
+// TODO: Save places to local storage and load them when the page comes up
+const handleAddClicked = () => {
+    const place = document.getElementById('place-input').value;
+    slices.push(place);
 }
