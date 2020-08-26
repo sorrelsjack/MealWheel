875ed3b2bc939cbe00b71b82327beb3c76a7376d
Jack Sorrels <sorrelsjack@gmail.com>
2020-08-26 15:31:51 -0400

    Code cleanup

--- script.js
+++ script.js
@@ -5,22 +5,10 @@ const svgNS = 'http://www.w3.org/2000/svg';
 let places = ['CFA', 'KFC'];
 let colors = ['#5390d9', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51', '#ef476f', '#bc00dd', '#6a00f4']; // https://coolors.co/264653-2a9d8f-e9c46a-f4a261-e76f51
 
-let cumulativePercent = 0;
-let firstX = 0;
-let firstY = 0;
-let secondX = 0;
-let secondY = 0;
-let firstAngle = 0;
-let secondAngle = 0;
+let cumulativePercent = firstAngle = secondAngle = 0;
 
 let clickDuration = 0;
 let clickDurationIntervalId = null;
-
-const getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 const getCoordinatesForPercent = (percent) => {
     const x = Math.cos(2 * Math.PI * percent);
@@ -46,23 +34,23 @@ const drawSlice = (percent, place, color) => {
     path.setAttribute('d', pathData);
     path.setAttribute('fill', color);
     path.setAttribute('id', `${place}-path`);
-    // TODO: Fix so text actually shows up
+    drawText(place);
+    svg.appendChild(path);
+}
+
+const drawText = (value) => {
     const text = document.createElement('div');
     text.setAttribute('class', 'circle-text');
     //text.setAttribute('style', `transform: rotate(-0.25turn)`)
     const textNode = document.createTextNode('Test');
     text.appendChild(textNode);
-    svg.appendChild(path);
     document.getElementById('circle-svg-container').insertBefore(text, document.getElementById('circle-svg-container').firstChild);
-}
-
-const drawText = (deg, text) => {
-
 }
 
 // TODO: Draw a whole circle if there's no slices defined
 // Credit: https://medium.com/hackernoon/a-simple-pie-chart-in-svg-dbdd653b6936
 const drawChart = () => {
+    resetDragValues();
     svg = document.getElementById('circle-svg');
 
     numberOfSlices = places.length;
@@ -73,18 +61,14 @@ const drawChart = () => {
     }
 
     draggableCircle = Draggable.create('#circle-svg-container', { type: 'rotation', dragResistance: 0 })[0];
-    // Angular velocity = change in angular displacement
+
     draggableCircle.addEventListener('press', () => {
         clickDurationIntervalId = window.setInterval(trackClickDuration, 1);
-        //firstX = e.offsetX;
-        //firstY = e.offsetY;
         firstAngle = draggableCircle.rotation;
     });
+
     draggableCircle.addEventListener('dragend', () => {
-        //if (!firstX && !firstY) return;
         window.clearInterval(clickDurationIntervalId);
-        //secondX = e.offsetX;
-        //secondY = e.offsetY;
         secondAngle = draggableCircle.endRotation;
 
         measureClickVelocity();
@@ -95,7 +79,7 @@ const drawChart = () => {
                 results.innerText = `Looks like you're eating at ${p}!`
                 results.style.visibility = 'visible';
             }
-        });  
+        });
     });
 }
 
@@ -104,30 +88,27 @@ const initialize = () => {
     drawChart();
 }
 
-const getColors = () => {
-    const numberOfColorSetsNeeded = Math.ceil(places.length / colors.length);
-    let sets = [];
-
-    for (let i = 0; i < numberOfColorSetsNeeded; i++) {
-        sets = sets.concat(colors);
-    }
-
-    return sets;
-}
-
-const calculateDistanceBetweenPoints = () => Math.sqrt(Math.pow((secondX - firstX), 2) + Math.pow((secondY - firstY), 2));
+const resetDragValues = () => clickDuration = firstAngle = secondAngle = 0;
 
 // https://stackoverflow.com/questions/52039421/java-2d-slow-down-rotation-like-a-wheel-of-fortune Hmmm
 const measureClickVelocity = () => {
-    //const distance = calculateDistanceBetweenPoints();
     const distance = secondAngle - firstAngle;
     clickVelocity = Math.abs(distance / clickDuration); // Number of revolutions per second
     //const acceleration = clickVelocity / clickDuration; // TODO: Continuously be calculating distance and duration and adjust accel
     // TODO: Get decel
     // TODO: 'Backwards' spins
-    // TODO: Fix issue where the it flashes
-    gsap.fromTo('#circle-svg-container', { rotation: draggableCircle.endRotation }, { rotation: draggableCircle.endRotation + (360 * clickVelocity * 2), duration: 2 });
-    clickDuration = secondX = firstX = secondY = firstY = firstAngle = secondAngle = 0;
+    gsap.fromTo(
+        '#circle-svg-container',
+        {
+            rotation: draggableCircle.endRotation
+        },
+        {
+            rotation: draggableCircle.endRotation + (360 * clickVelocity * 3),
+            duration: 3,
+            onInterrupt: () => resetDragValues()
+        });
+
+    resetDragValues();
 }
 
 const resetChart = () => {
