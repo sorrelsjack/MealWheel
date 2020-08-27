e016443814c1399739d4b200ba33d04a33b66269
Jack Sorrels <sorrelsjack@gmail.com>
2020-08-27 17:46:23 -0400

    The text goes inside the wheel now... Amazing

--- script.js
+++ script.js
@@ -5,59 +5,89 @@ const svgNS = 'http://www.w3.org/2000/svg';
 let places = ['CFA', 'KFC'];
 let colors = ['#5390d9', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51', '#ef476f', '#bc00dd', '#6a00f4']; // https://coolors.co/264653-2a9d8f-e9c46a-f4a261-e76f51
 
-let cumulativePercent = firstAngle = secondAngle = 0;
+let firstAngle = secondAngle = 0;
 
 let clickDuration = 0;
 let clickDurationIntervalId = null;
 
-const getCoordinatesForPercent = (percent) => {
-    const x = Math.cos(2 * Math.PI * percent);
-    const y = Math.sin(2 * Math.PI * percent);
-
-    return [x, y];
-}
-
-const drawSlice = (percent, place, color) => {
-    const [startX, startY] = getCoordinatesForPercent(cumulativePercent);
-    cumulativePercent += percent;
-    const [endX, endY] = getCoordinatesForPercent(cumulativePercent);
-
-    const largeArcFlag = percent > .5 ? 1 : 0;
-
-    const pathData = [
-        `M ${startX} ${startY}`,
-        `A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY}`,
-        `L 0 0`,
-    ].join(' ');
-
-    const path = document.createElementNS(svgNS, 'path');
-    path.setAttribute('d', pathData);
-    path.setAttribute('fill', color);
-    path.setAttribute('id', `${place}-path`);
-    drawText(place);
-    svg.appendChild(path);
-}
-
-const drawText = (value) => {
-    const text = document.createElement('div');
-    text.setAttribute('class', 'circle-text');
-    //text.setAttribute('style', `transform: rotate(-0.25turn)`)
-    const textNode = document.createTextNode('Test');
-    text.appendChild(textNode);
-    document.getElementById('circle-svg-container').insertBefore(text, document.getElementById('circle-svg-container').firstChild);
-}
-
 // TODO: Draw a whole circle if there's no slices defined
-// Credit: https://medium.com/hackernoon/a-simple-pie-chart-in-svg-dbdd653b6936
+// Credit: https://bufferwall.com/posts/330881001ji1a/
 const drawChart = () => {
     resetDragValues();
     svg = document.getElementById('circle-svg');
 
-    numberOfSlices = places.length;
-    slicePercentage = 1 / numberOfSlices;
+    const cx = 300;
+    const cy = 300;
+    const radius = 300;
 
-    for (let i = 0; i < numberOfSlices; i++) {
-        drawSlice(slicePercentage, places[i], colors[i]);
+    let percent = 100 / places.length;
+    let total = 0;
+    var offset = 0;
+    var offset2 = 0;
+    var arr = [];
+    var x;
+    var y;
+    var la;
+    var radians;
+
+    for (var i = 0; i < places.length; i++)
+        total += percent;
+
+    for (var i = 0; i < places.length; i++) {
+
+        var item = places[i];
+        var tmp = {};
+
+        tmp.index = i;
+        tmp.value = item;
+        radians = (((percent / total) * 360) * Math.PI) / 180;
+        offset2 = ((offset / total) * 360);
+        tmp.data = item;
+
+        x = cx + Math.sin(radians) * radius;
+        y = cy - Math.cos(radians) * radius;
+        la = radians > Math.PI ? 1 : 0;
+
+        // Arc
+        tmp.d = `M${cx} ${cy},L${cx} ${cy - radius},A${radius} ${radius},0 ${la} 1,${x} ${y}Z`;
+        tmp.transform = `rotate(${offset2}, ${cx}, ${cy})`;
+
+        // Text
+        x = cx + Math.sin(radians / 2) * radius / 2;
+        y = cy - Math.cos(radians / 2) * radius / 2;
+
+        tmp.xValue = x;
+        tmp.yValue = y;
+        tmp.transformValue = `rotate(${-offset2},${x},${y})`;
+
+        offset += percent;
+        arr.push(tmp);
+    }
+
+    for (let i = 0; i < arr.length; i++){
+        let item = arr[i];
+        const group = document.createElementNS(svgNS, 'g');
+        group.setAttribute('transform', `${item.transform}`);
+
+        const path = document.createElementNS(svgNS, 'path');
+        path.setAttribute('id', `${item.value}-path`);
+        path.setAttribute('d', `${item.d}`);
+        path.setAttribute('fill', `${colors[i]}`);
+
+        group.appendChild(path);
+
+        const text = document.createElementNS(svgNS, 'text');
+        text.setAttribute('fill', 'white'); 
+        text.setAttribute('font-size', 30);
+        text.setAttribute('x', item.xValue);
+        text.setAttribute('y', item.yValue);
+        text.setAttribute('transform', item.transformValue);
+
+        const textNode = document.createTextNode(item.value);
+        text.appendChild(textNode);
+
+        group.appendChild(text);
+        svg.appendChild(group);
     }
 
     draggableCircle = Draggable.create('#circle-svg-container', { type: 'rotation', dragResistance: 0 })[0];
@@ -113,13 +143,15 @@ const measureClickVelocity = () => {
 
 const resetChart = () => {
     clearInterval();
+    // TODO: Delete all text
+    document.getElementById('circle-svg').innerHTML = '';
     drawChart();
 }
 
 // TODO: Fix issue where input is unclickable
 // TODO: Add ability to remove items
 // TODO: In local storage, maybe add a param to track how many times a place has come up, and store if its 'active' or not
-// TODO: 'Sub' previous items back into teh list?
+// TODO: 'Sub' previous items back into the list?
 const addItemToList = (value) => {
     const placeList = document.getElementById('place-list');
     const placeListItem = document.createElement('li');
